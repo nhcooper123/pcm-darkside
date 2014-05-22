4af8cf23a4cd2bffd978ff21447f7e15b7f3e33b
Natalie Cooper <ncooper@tcd.ie>
2014-05-22 23:19:34 +0100

    First attempt at citations through time graph for all papers

---
+++ Analyses/Figure_CitationsPerYear.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+library(reshape)
+
+ds <- read.csv("CitesThruTime.csv")
+
+# Reshape the data so it's in long format
+data_long <- melt(ds, id = "Year")
+
+pdf("CitationsPerYear_AllPapers")
+
+# Graph showing how citations change through time
+# for key PCM papers
+myplot <- ggplot(data = data_long,
+                 aes(x = Year, y = value, colour = variable))
+    
+# Modifications to graph including legend, y-axis label etc.
+myplot + 
+geom_line() + 
+guides(col = guide_legend(nrow = 14, title = "Paper"), size = 0.5) +
+ylab("Citations")
+
+dev.off()
+
+# Need to limit to fewer papers to get a clearer graph
