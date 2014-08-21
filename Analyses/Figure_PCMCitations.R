c603a2ada0ed8f43bc1c1c662364e1a1d2a27c09
Natalie Cooper <ncooper@tcd.ie>
2014-08-21 20:30:10 +0100

    Fixed draft figure 1

--- Analyses/Figure_PCMCitations.R
+++ Analyses/Figure_PCMCitations.R
@@ -1,12 +1,10 @@
 ds <- read.csv("PCMCitesThruTime.csv")
 
-# Plot for talk slides
-newred <- rgb(red = 235, green = 81, blue = 83, maxColorValue = 255)
-
-png("PCMCitations.png", bg = "transparent")
+png("PCMCitations.png")
 
 # Plot citations
+par(bty = "l")
 plot(Citations ~ Year, data = ds, type = "l", ylim = c(0, 800), 
-     col = newred, lwd = 5)
+	 las = 1, lwd = 2, cex.lab = 1.2, cex.axis = 1.2)
 
 dev.off()
