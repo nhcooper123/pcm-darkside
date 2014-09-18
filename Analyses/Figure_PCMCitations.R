14b5146a906c9e1d43a9babb5f4d0bc809a3fa3a
Natalie Cooper <ncooper@tcd.ie>
2014-09-18 16:13:01 +0100

    Altered y axis label of draft Fig 1

--- Analyses/Figure_PCMCitations.R
+++ Analyses/Figure_PCMCitations.R
@@ -5,6 +5,7 @@ png("PCMCitations.png")
 # Plot citations
 par(bty = "l")
 plot(Citations ~ Year, data = ds, type = "l", ylim = c(0, 800), 
-	 las = 1, lwd = 2, cex.lab = 1.2, cex.axis = 1.2)
+	 las = 1, lwd = 2, cex.lab = 1.2, cex.axis = 1.2, 
+	 ylab = "Papers", xlab = "Year")
 
 dev.off()
