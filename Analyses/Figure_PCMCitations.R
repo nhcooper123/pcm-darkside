ee8b637d1e80d3d802f56cae2a9787e661ac16d7
Natalie Cooper <ncooper@tcd.ie>
2015-04-15 15:01:58 +0100

    Updated data and analyses for talk and paper

--- Analyses/Figure_PCMCitations.R
+++ Analyses/Figure_PCMCitations.R
@@ -1,11 +1,15 @@
-ds <- read.csv("PCMCitesThruTime.csv")
+# For talk
 
-png("PCMCitations.png")
+ds <- read.csv("Data/PCMCitesThruTime.csv", )
+
+newred <- rgb(red = 235, green = 81, blue = 83, maxColorValue = 255)
+
+png("Figures/PCMCitationsTalk.png", bg = "transparent")
 
 # Plot citations
 par(bty = "l")
 plot(Citations ~ Year, data = ds, type = "l", ylim = c(0, 800), 
-	 las = 1, lwd = 2, cex.lab = 1.2, cex.axis = 1.2, 
+	 las = 1, lwd = 2, cex.lab = 1.2, cex.axis = 1.2, col = newred,
 	 ylab = "Papers", xlab = "Year")
 
 dev.off()
