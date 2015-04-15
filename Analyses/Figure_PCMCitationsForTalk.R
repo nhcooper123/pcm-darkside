4fd9e7ae2f26b6ae0d208c590e361c9488ad898d
Natalie Cooper <ncooper@tcd.ie>
2015-04-15 15:13:05 +0100

    Thicker line for talk fig 1

--- Analyses/Figure_PCMCitationsForTalk.R
+++ Analyses/Figure_PCMCitationsForTalk.R
@@ -9,7 +9,7 @@ png("Manuscript/Figures/PCMCitations_Talk.png", bg = "transparent")
 # Plot citations
 par(bty = "l")
 plot(Citations ~ Year, data = ds, type = "l", ylim = c(0, 800), 
-	 las = 1, lwd = 2, cex.lab = 1.2, cex.axis = 1.2, col = newred,
+	 las = 1, lwd = 5, cex.lab = 1.2, cex.axis = 1.2, col = newred,
 	 ylab = "Papers", xlab = "Year")
 
 dev.off()
